73bab6891541ec9f2aff13d6e6d6a0bddff31a77
akhmadfaizabdulloh <akhmadfaizabdulloh@gmail.com>
2024-01-30 14:54:12 +0700

    layoting system

--- resources/views/posts.blade.php
+++ resources/views/posts.blade.php
@@ -1,11 +1,8 @@
-<!DOCTYPE html>
-<html lang="en">
-<head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Faiz Blog | Posts</title>
-</head>
-<body>
+@extends('layouts.main')
+
+@section('container')
+
     <h1>Halaman Posts</h1>
-</body>
-</html>
+
+@endsection
+ 
