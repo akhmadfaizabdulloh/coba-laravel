6806b52f600f2a036e58c32612b9a8e9a5fcb1e0
akhmadfaizabdulloh <akhmadfaizabdulloh@gmail.com>
2024-02-01 18:29:06 +0700

    title dan navigasi dinamis

--- routes/web.php
+++ routes/web.php
@@ -35,11 +35,14 @@ use Illuminate\Support\Facades\Route;
 
 
 Route::get('/', function () {
-    return view('home');
+    return view('home', [
+        "title" => "Home"
+    ]);
 });
 
 Route::get('/about', function () {
     return view('about', [
+        "title" => "About",
         "name" => "Akhmad Faiz Abdulloh",
         "email" => "akhmadfaizabdulloh@gmail.com",
         "image" => "cute.jpeg"
@@ -47,5 +50,7 @@ Route::get('/about', function () {
 });
 
 Route::get('/blog', function () {
-    return view('posts');
+    return view('posts', [
+        "title" => "Posts"
+    ]);
 });
