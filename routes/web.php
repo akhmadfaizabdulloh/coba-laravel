599135d925ef894982e5222423b3ab8e093fb35c
akhmadfaizabdulloh <akhmadfaizabdulloh@gmail.com>
2024-02-03 16:50:20 +0700

    single post - 4. Blade Templating Engine (Done)

--- routes/web.php
+++ routes/web.php
@@ -55,11 +55,13 @@ Route::get('/blog', function () {
     $blog_posts = [
         [
             "title" => "Judul Post Pertama",
+            "slug" => "judul-post-pertama",
             "author" => "Sandhika Galih",
             "body" => "Lorem ipsum dolor sit amet consectetur adipisicing elit. Repellat explicabo magnam natus vitae totam et hic est asperiores ex ducimus ipsa quis tempora, porro nobis, quidem, nesciunt numquam veniam repudiandae corporis. Facere deserunt quam tempore, ipsam consequatur dignissimos ipsa, nisi voluptas non voluptatum cumque recusandae porro quos pariatur cum sed explicabo quae provident exercitationem temporibus delectus tenetur. Voluptatibus nesciunt sapiente obcaecati nisi illo explicabo maxime enim perspiciatis asperiores et, vitae itaque ducimus, natus iusto cumque impedit! Consequuntur accusamus laborum reiciendis!"
         ],
         [
             "title" => "Judul Post Kedua",
+            "slug" => "judul-post-kedua",
             "author" => "Doddy Ferdiansyah",
             "body" => "Lorem ipsum dolor sit amet consectetur adipisicing elit. Quaerat nulla sint dolores rerum a, hic totam rem, reprehenderit minima itaque minus esse exercitationem quo. Quia, dolore vel quo asperiores iure quis odio ut dicta velit magni harum aliquid atque magnam! Eos sint expedita neque! Dolorem earum deserunt aliquid minus tenetur, culpa adipisci recusandae vero, fugit consequatur accusantium eius voluptates fugiat magnam saepe excepturi, nam iste animi architecto dignissimos odio quo! Veniam ut ipsam quam provident delectus tenetur beatae aliquid ab alias sapiente reiciendis explicabo laborum perferendis sint consectetur, esse voluptate. Id laborum, repudiandae facere inventore corporis ab nobis amet eius."
         ],
@@ -69,4 +71,35 @@ Route::get('/blog', function () {
         "title" => "Posts",
         "posts" => $blog_posts
     ]);
+});
+
+
+// halaman single post
+Route::get('posts/{slug}', function ($slug) {
+    $blog_posts = [
+        [
+            "title" => "Judul Post Pertama",
+            "slug" => "judul-post-pertama",
+            "author" => "Sandhika Galih",
+            "body" => "Lorem ipsum dolor sit amet consectetur adipisicing elit. Repellat explicabo magnam natus vitae totam et hic est asperiores ex ducimus ipsa quis tempora, porro nobis, quidem, nesciunt numquam veniam repudiandae corporis. Facere deserunt quam tempore, ipsam consequatur dignissimos ipsa, nisi voluptas non voluptatum cumque recusandae porro quos pariatur cum sed explicabo quae provident exercitationem temporibus delectus tenetur. Voluptatibus nesciunt sapiente obcaecati nisi illo explicabo maxime enim perspiciatis asperiores et, vitae itaque ducimus, natus iusto cumque impedit! Consequuntur accusamus laborum reiciendis!"
+        ],
+        [
+            "title" => "Judul Post Kedua",
+            "slug" => "judul-post-kedua",
+            "author" => "Doddy Ferdiansyah",
+            "body" => "Lorem ipsum dolor sit amet consectetur adipisicing elit. Quaerat nulla sint dolores rerum a, hic totam rem, reprehenderit minima itaque minus esse exercitationem quo. Quia, dolore vel quo asperiores iure quis odio ut dicta velit magni harum aliquid atque magnam! Eos sint expedita neque! Dolorem earum deserunt aliquid minus tenetur, culpa adipisci recusandae vero, fugit consequatur accusantium eius voluptates fugiat magnam saepe excepturi, nam iste animi architecto dignissimos odio quo! Veniam ut ipsam quam provident delectus tenetur beatae aliquid ab alias sapiente reiciendis explicabo laborum perferendis sint consectetur, esse voluptate. Id laborum, repudiandae facere inventore corporis ab nobis amet eius."
+        ],
+    ];
+    
+    $new_post = [];
+    foreach ($blog_posts as $post) {
+        if ($post["slug"] === $slug) {
+            $new_post = $post;
+        }
+    }
+
+    return view('post', [
+        "title" => "Single Post",
+        "post" => $new_post
+    ]); 
 });
