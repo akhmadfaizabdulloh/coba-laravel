49b950051f8824e659b62cd680da3272b8bbada2
akhmadfaizabdulloh <akhmadfaizabdulloh@gmail.com>
2024-02-17 18:37:29 +0700

    video ke-5 - menggunakan collection finish

--- app/Models/Post.php
+++ app/Models/Post.php
@@ -14,7 +14,7 @@ class Post
 {
     private static $blog_posts = [
         [
-            "title" => "Judul Post Sandhika",
+            "title" => "Judul Post Pertama",
             "slug" => "judul-post-pertama",
             "author" => "Sandhika Galih",
             "body" => "Lorem ipsum dolor sit amet consectetur adipisicing elit. Repellat explicabo magnam natus vitae totam et hic est asperiores ex ducimus ipsa quis tempora, porro nobis, quidem, nesciunt numquam veniam repudiandae corporis. Facere deserunt quam tempore, ipsam consequatur dignissimos ipsa, nisi voluptas non voluptatum cumque recusandae porro quos pariatur cum sed explicabo quae provident exercitationem temporibus delectus tenetur. Voluptatibus nesciunt sapiente obcaecati nisi illo explicabo maxime enim perspiciatis asperiores et, vitae itaque ducimus, natus iusto cumque impedit! Consequuntur accusamus laborum reiciendis!"
@@ -29,20 +29,24 @@ class Post
 
     public static function all()
     {
-        return self::$blog_posts;
+        return collect(self::$blog_posts);
     }
 
     public static function find($slug)
     {
-        $posts = self::$blog_posts; 
-        $post = [];
-        foreach ($posts as $p) {
-            if ($p["slug"] === $slug) {
-                $post = $p;
-            }
-        }
+        // $posts = self::$blog_posts; 
+        $posts = static::all();
+
+        // $post = [];
+        // foreach ($posts as $p) {
+        //     if ($p["slug"] === $slug) {
+        //         $post = $p;
+        //     }
+        // }
         
-        return $post;
+        // return $posts->first(); //untuk mengambil data yg pertama (semua postingan)
+
+        return $posts->firstWhere('slug', $slug); //ambil satu data yg pertama kali ditemukan, dimana slugnya = slug
 
     }
 }
