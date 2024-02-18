053cd232715fe8913c52d52fb3f349cf43b9eb33
akhmadfaizabdulloh <akhmadfaizabdulloh@gmail.com>
2024-02-18 23:52:34 +0700

    video ke-5 - membuat controller finish

--- routes/web.php
+++ routes/web.php
@@ -1,5 +1,6 @@
 <?php
 
+use App\Http\Controllers\PostController;
 use App\Models\Post;
 use Illuminate\Support\Facades\Route;
 
@@ -52,18 +53,5 @@ Route::get('/about', function () {
 
 
 
-Route::get('/posts', function () {
-    return view('posts', [
-        "title" => "Posts",
-        "posts" => Post::all()
-    ]);
-});
-
-
-// halaman single post
-Route::get('posts/{slug}', function ($slug) {
-    return view('post', [
-        "title" => "Single Post",
-        "post" => Post::find($slug)
-    ]); 
-});
+Route::get('/posts', [PostController::class, 'index']);
+Route::get('posts/{slug}', [PostController::class, 'show']); // halaman single post
