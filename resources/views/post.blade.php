ba0e57ddb3168e281244dd85022c23312212e954
akhmadfaizabdulloh <akhmadfaizabdulloh@gmail.com>
2024-11-05 00:28:36 +0700

    Finishing video 13 - Searching & Pagination

--- resources/views/post.blade.php
+++ resources/views/post.blade.php
@@ -9,7 +9,7 @@
 
                 <h1 class="mb-3">{{ $post->title }}</h1>
 
-                <p>By. <a href="/authors/{{ $post->author->username }}" class="text-decoration-none">{{ $post->author->name }}</a> in <a href="/categories/{{ $post->category->slug }}" class="text-decoration-none">{{ $post->category->name }}</a></p>
+                <p>By. <a href="/posts?author={{ $post->author->username }}" class="text-decoration-none">{{ $post->author->name }}</a> in <a href="/posts?category={{ $post->category->slug }}" class="text-decoration-none">{{ $post->category->name }}</a></p>
                 
                 <img src="/img/{{ $post->category->slug }}.jpg" width="1200" height="400" style="object-fit: cover; object-position: center;" class="img-fluid" alt="{{ $post->category->name }}">
 
--- resources/views/posts.blade.php
+++ resources/views/posts.blade.php
@@ -9,6 +9,16 @@
     <div class="row justify-content-center mb-3">
         <div class="col-md-6">
             <form action="/posts" method="GET">
+                {{-- method-nya defult "GET" apabila dikosongi --}}
+                
+                @if (request('category'))
+                    <input type="hidden" name="category" value="{{ request('category') }}">
+                @endif
+
+                @if (request('author'))
+                    <input type="hidden" name="author" value="{{ request('author') }}">
+                @endif
+
                 <div class="input-group mb-3">
                     <input type="text" class="form-control" placeholder="Search.." name="search" value="{{ request('search') }}">
                     <button class="btn btn-danger" type="submit">Search</button>
@@ -27,7 +37,7 @@
                 </h3>
                 <p>
                     <small class="text-muted">
-                    By. <a href="/authors/{{ $posts[0]->author->username }}" class="text-decoration-none">{{ $posts[0]->author->name }}</a> in <a href="/categories/{{ $posts[0]->category->slug }}" class="text-decoration-none">{{ $posts[0]->category->name }}</a>
+                    By. <a href="/posts?author={{ $posts[0]->author->username }}" class="text-decoration-none">{{ $posts[0]->author->name }}</a> in <a href="/posts?category={{ $posts[0]->category->slug }}" class="text-decoration-none">{{ $posts[0]->category->name }}</a>
                     {{ $posts[0]->created_at->diffForHumans() }}
                     </small>
                 </p>
@@ -45,7 +55,7 @@
                         <div class="card">
 
                             <div class="position-absolute px-3 py-2 text-white" style="background-color:rgba(0, 0, 0, 0.7)">
-                                <a href="/categories/{{ $post->category->slug }}" class="text-white text-decoration-none">
+                                <a href="/posts?category={{ $post->category->slug }}" class="text-white text-decoration-none">
                                     {{ $post->category->name }}
                                 </a>
                             </div>
@@ -55,7 +65,7 @@
                                 <h5 class="card-title"><a href="/posts/{{ $post->slug }}" class="text-decoration-none text-dark">{{ $post->title }}</a></h5>
                                 <p>
                                     <small class="text-muted">
-                                    By. <a href="/authors/{{ $post->author->username }}" class="text-decoration-none">{{ $post->author->name }}</a> 
+                                    By. <a href="/posts?author={{ $post->author->username }}" class="text-decoration-none">{{ $post->author->name }}</a> 
                                     {{ $post->created_at->diffForHumans() }}
                                     </small>
                                 </p>
@@ -90,6 +100,10 @@
         {{-- kondisi jika false --}}
         <p class="text-center fs-4">No post found.</p>
     @endif
+
+    <div class="d-flex justify-content-end">
+        {{ $posts->links() }}
+    </div>
 
 @endsection
  
