01ba71f4f012e504ba5b775b524dbd2ba981f0c3
akhmadfaizabdulloh <akhmadfaizabdulloh@gmail.com>
2024-11-07 04:32:10 +0700

    Finishing video 14 - View Login & Registration

--- routes/web.php
+++ routes/web.php
@@ -2,6 +2,7 @@
 
 use App\Http\Controllers\PostController;
 use App\Http\Controllers\LoginController;
+use App\Http\Controllers\RegisterController;
 use App\Models\Category;
 use Illuminate\Support\Facades\Route;
 
@@ -89,4 +90,7 @@ Route::get('/categories', function() {
 
 
 Route::get('/login', [LoginController::class, 'index']);
+
+Route::get('/register', [RegisterController::class, 'index']);
+
 
