924347c2860720f410b1165b84cbc31981ceb799
akhmadfaizabdulloh <akhmadfaizabdulloh@gmail.com>
2024-11-18 21:05:30 +0700

    video 16 - otentikasi login

--- app/Http/Controllers/LoginController.php
+++ app/Http/Controllers/LoginController.php
@@ -3,6 +3,10 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Auth;
+
+// use Illuminate\Support\Facades\Auth;
+
 
 class LoginController extends Controller
 {
@@ -12,5 +16,22 @@ class LoginController extends Controller
             'title' => 'Login',
             'active' => 'login'
         ]);
+    }
+
+    public function authenticate(Request $request)
+    {
+        $credentials = $request->validate([
+            'email' => 'required|email:dns',
+            'password' => 'required'
+        ]);
+
+        // dd('berhasil login!');
+
+        if (Auth::attempt($credentials)) {
+            $request->session()->regenerate();
+            return redirect()->intended('/dashboard');
+        }
+
+        return back()->with('loginError', 'Login failed!');
     }
 }
--- resources/views/login/index.blade.php
+++ resources/views/login/index.blade.php
@@ -14,30 +14,48 @@
 
         @endif
 
+        @if(session()->has('loginError'))
+
+            <div class="alert alert-danger alert-dismissible fade show" role="alert">
+                {{ session('loginError') }}
+                <button type="button" class="btn-close" data-bs-dismiss="alert" aria-label="Close"></button>
+            </div>
+
+        @endif
+
         <main class="form-signin">
             {{-- <img class="mb-4" src="../assets/brand/bootstrap-logo.svg" alt="" width="72" height="57"> --}}
             <h1 class="h3 mb-3 fw-normal text-center">Please Login</h1>
 
-                <form>
+                <form action="/login" method="post">
+                    @csrf
           
-                <div class="form-floating">
-                    <input type="email" class="form-control" id="floatingInput" placeholder="name@example.com">
-                    <label for="floatingInput">Email address</label>
-                </div>
-                <div class="form-floating">
-                    <input type="password" class="form-control" id="floatingPassword" placeholder="Password">
-                    <label for="floatingPassword">Password</label>
-                </div>
-            
-                {{-- <div class="checkbox mb-3">
-                    <label>
-                    <input type="checkbox" value="remember-me"> Remember me
-                    </label>
-                </div> --}}
+                    <div class="form-floating">
+                        <input type="email" name="email" class="form-control @error('email') is-invalid @enderror" id="email" placeholder="name@example.com" autofocus required value="{{ old('email') }}">
+                        <label for="email">Email address</label>
+                        
+                        @error('email')
+                        <div class="invalid-feedback">
+                            {{ $message }}
+                        </div>
+                        @enderror
 
-                <button class="w-100 btn btn-lg btn-primary" type="submit">Login</button>
-                {{-- <p class="mt-5 mb-3 text-muted">&copy; 2017–2021</p> --}}
-            </form>
+                    </div>
+                    <div class="form-floating">
+                        <input type="password" name="password" class="form-control" id="password" placeholder="Password" required>
+                        <label for="password">Password</label>
+                    </div>
+                
+                    {{-- <div class="checkbox mb-3">
+                        <label>
+                        <input type="checkbox" value="remember-me"> Remember me
+                        </label>
+                    </div> --}}
+
+                    <button class="w-100 btn btn-lg btn-primary" type="submit">Login</button>
+                    {{-- <p class="mt-5 mb-3 text-muted">&copy; 2017–2021</p> --}}
+                </form>
+                
             <small class="d-block text-center mt-3">Not registed? <a href="/register">Register Now!</a></small>
         </main>
     </div>
