eae2e3e239266ddce1dc2217d83065f49f781ee5
fafaiez <akhmadfaizabdulloh@gmail.com>
2025-02-13 11:04:07 +0700

    video 22 - membuat fitur preview image

--- resources/views/dashboard/posts/create.blade.php
+++ resources/views/dashboard/posts/create.blade.php
@@ -54,7 +54,10 @@
 
         <div class="mb-3">
           <label for="image_post" class="form-label">Post Image</label>
-          <input class="form-control @error('image_post') is-invalid @enderror" type="file" id="image_post" name="image_post">
+
+          <img class="img-preview img-fluid mb-3 col-sm-5">
+
+          <input class="form-control @error('image_post') is-invalid @enderror" type="file" id="image_post" name="image_post" onchange="previewImage()">
 
           @error('image_post')
               <div class="invalid-feedback">
@@ -106,7 +109,21 @@
 
   document.addEventListener('trix-file-accept', function(e) {
     e.preventDefault();
-  })
+  });
+
+  function previewImage() {
+    const image = document.querySelector('#image_post');
+    const imgPreview = document.querySelector('.img-preview');
+
+    imgPreview.style.display = 'block';
+
+    const oFReader = new FileReader();
+    oFReader.readAsDataURL(image.files[0]);
+
+    oFReader.onload = function(oFREvent) {
+      imgPreview.src = oFREvent.target.result;
+    }
+  }
 
 </script>
 
